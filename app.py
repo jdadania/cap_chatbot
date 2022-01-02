f507da118f95b16472fe2c4c897e0c3d697ff1ca
jdadania <76833342+jdadania@users.noreply.github.com>
2022-01-02 13:00:07 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,35 @@
 import pickle
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import contractions
+from autocorrect import Speller
+spell = Speller(lang='en')
+text_preprocessing=pickle.load(open('text.pkl','rb'))
 
+def get_tokenizer(text):
+    tokenizer = Tokenizer(num_words=5000)
+    tokenizer.fit_on_texts(text)
+    return tokenizer.texts_to_sequences(text)
+
+
+
+maxlen = 100
+def get_pad_sequences(text):
+    return pad_sequences(text, padding='post', maxlen=maxlen)
+	
+
+def predict_class(text_pad, clf):
+    return clf.predict(text_pad)
+
+	
+
+def chatbot_response(text):
+    text_tokenizer = get_tokenizer(text)
+    text_pad = get_pad_sequences(text_tokenizer)
+	respond = predict_class(text_pad, clf)
+    return respond
+	
+	
 from flask import Flask, render_template, request
 
 # load the model from disk
@@ -18,8 +48,9 @@ def home():
 @app.route("/get")
 def get_bot_response():
   userText = request.args.get('msg')
-  my_prediction = clf.predict(userText)
-  return my_prediction
+  text = text_preprocessing.text(userText)
+  #my_prediction = clf.predict(userText)
+  return chatbot_response(text)
    
 
 if __name__ == "__main__":
